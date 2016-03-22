5c61ee82af3a705dfafea0d85bcb481be7f8937b
Kevin Tai <kevin.r.tai@gmail.com>
2016-03-22 13:17:31 -0500

    Solved 3

--- 003/sol.hs
+++ 003/sol.hs
@@ -1,8 +1,11 @@
 main = do 
-    
+    let n = 600851475143
+    print $ maximum [x | x <- factors n, isPrime x]
 
-isPrime :: (Integral a) => a -> Bool
-isPrime n =
+isPrime :: Integer -> Bool
+isPrime n = length (factors n) == 2
 
-factors :: (Integral a) => a -> [Int]
-factors n = 
+factors :: Integer -> [Integer]
+factors n = let s = ceiling $ sqrt $ fromIntegral n
+                lower = [x | x <- [1..s], n `mod` x == 0]
+            in lower ++ [n `quot` y | y <- lower]
