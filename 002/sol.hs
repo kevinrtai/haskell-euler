57ac2dd7cd396dbd73ebc357683f0c5a45bbc21f
Kevin Tai <kevin.r.tai@gmail.com>
2016-03-22 00:44:45 -0500

    More efficient, DP like solution for Fibonnacci sequence. Thanks Sid!

--- 002/sol.hs
+++ 002/sol.hs
@@ -1,8 +1,6 @@
 main = do
-    let ans = sum [x | x <- (takeWhile (<4000000) (map fib [1..])), even x]
+    let ans = sum [x | x <- (takeWhile (<4000000) fib), even x]
     print(ans)
 
-fib :: (Integral a) => a -> Int
-fib 1 = 1
-fib 2 = 2
-fib n = fib (n-1) + fib (n-2)
+fib :: [Int]
+fib = 0:1:zipWith (+) fib (tail fib)
