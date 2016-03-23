bcc7d4f48273a60a40d9332b12e61cd424e747ae
Kevin Tai <kevin.r.tai@gmail.com>
2016-03-23 01:28:41 -0500

    Added solution for 4

--- 004/sol.hs
+++ 004/sol.hs
@@ -1,2 +1,13 @@
+import Data.List
+
 main = do
-    putStrLn "Dummy"
+    let triple = [x * y | x <- [100..999], y <- [100..999]]
+    print $ maximum [x | x <- triple, isPalindrome x]
+
+isPalindrome :: Int -> Bool
+isPalindrome n = d == (reverse d)
+    where d = digits n []
+
+digits :: Int -> [Int] -> [Int]
+digits 0 xs = xs
+digits n xs = (n `mod` 10):(digits (quot n 10) xs)
