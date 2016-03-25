1907a7f267533dd39577a0ad04102cb2eff053de
Kevin Tai <kevin.r.tai@gmail.com>
2016-03-24 23:31:13 -0500

    Cleaned up solution for 8

--- 008/sol.hs
+++ 008/sol.hs
@@ -3,9 +3,9 @@ import Data.Char
 main = do
     numStr <- readFile "./num.txt"
     let num = init numStr
-    print $ maximum $ prodThirteen num []
+    print $ maximum $ prodThirteen num
 
-prodThirteen :: String -> [Int] -> [Int]
-prodThirteen "" xs = xs
-prodThirteen str xs = prodThirteen (tail str) ((product digits):xs)
+prodThirteen :: String -> [Int]
+prodThirteen "" = [0]
+prodThirteen str= (product digits):prodThirteen (tail str)
         where digits = [digitToInt x | x <- take 13 str]
